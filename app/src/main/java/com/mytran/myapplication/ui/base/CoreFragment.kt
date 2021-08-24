1176d8454923e2984f096b4b5966e10dbbb01a68
mytran <mtoanmy91@gmail.com>
2021-08-25 02:41:10 +0700

    [INIT] update structures

---
+++ app/src/main/java/com/mytran/myapplication/ui/base/CoreFragment.kt
@@ -0,0 +1,23 @@
+package com.mytran.myapplication.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+abstract class CoreFragment: Fragment() {
+    abstract fun getLayoutId(): Int
+    abstract fun initObserver()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(getLayoutId(), container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initObserver()
+    }
+}
--- app/src/main/java/com/mytran/myapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/mytran/myapplication/ui/main/MainFragment.kt
@@ -1,38 +1,20 @@
 package com.mytran.myapplication.ui.main
 
-import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.mytran.myapplication.R
+import com.mytran.myapplication.ui.base.CoreFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainFragment : Fragment() {
-
+class MainFragment : CoreFragment() {
     companion object {
         fun newInstance() = MainFragment()
     }
-
     private val homeViewModel: MainViewModel by viewModel()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        initObserver()
-    }
-
-    private fun initObserver () {
+    override fun getLayoutId(): Int = R.layout.main_fragment
+    override fun initObserver() {
         homeViewModel.getCoinList()
-        homeViewModel.coinList.observe(viewLifecycleOwner, Observer { coinList ->
+        homeViewModel.coinList.observe(viewLifecycleOwner, { coinList ->
             Log.v("MainFragment", "coinList $coinList")
         })
     }
