cb5b06e1021e760fd5decf76755749d61a6edbee
mytran <mtoanmy91@gmail.com>
2021-09-20 16:04:33 +0700

    test

--- app/src/main/java/com/mytran/myapplication/utils/TestCodeReview.kt
+++ app/src/main/java/com/mytran/myapplication/utils/TestCodeReview.kt
@@ -2,6 +2,17 @@ package com.mytran.myapplication.utils
 
 import java.io.IOException
 
+class OutOfOrder {
+    companion object {
+        const val IMPORTANT_VALUE = 3
+    }
+
+    fun returnX(): Int {
+        return x
+    }
+
+    private val x = 2
+}
 class TestCodeReview {
     override fun toString(): String {
         throw IllegalStateException() // exception should not be thrown here
