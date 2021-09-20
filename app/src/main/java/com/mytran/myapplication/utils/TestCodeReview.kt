e9972e011cfc77aecda6d5cb1dc591a112c3bd2f
mytran <mtoanmy91@gmail.com>
2021-09-20 15:45:32 +0700

    test

--- app/src/main/java/com/mytran/myapplication/utils/TestCodeReview.kt
+++ app/src/main/java/com/mytran/myapplication/utils/TestCodeReview.kt
@@ -1,7 +1,16 @@
 package com.mytran.myapplication.utils
 
+import java.io.IOException
+
 class TestCodeReview {
     override fun toString(): String {
         throw IllegalStateException() // exception should not be thrown here
+    }
+    fun foo() {
+        try {
+            // ... do some I/O
+        } catch(e: IOException) {
+            if (e is NoSuchMethodException || (e as NoSuchMethodException) != null) { }
+        }
     }
 }
